4193e1d6b4b5a9d30af88d96bb0a5aac66fdaf5f
Carlos Carrillo <90294685+cjst3ch@users.noreply.github.com>
2022-06-22 20:02:52 +0200

    added getItem, added order var PP all

--- src/public/scripts/checkout.js
+++ src/public/scripts/checkout.js
@@ -4,6 +4,7 @@ var order = []
 var cashButton = document.getElementById("cash-button")
 var cardButton = document.getElementById("card-button")
 var productSku = document.getElementById("product-sku")
+var itemQuantity = document.getElementById("item-quantity")
 
 function onCash() {
     cash = true
@@ -16,3 +17,28 @@ function onCard() {
     cashButton.classList.remove("btn-primary")
     cardButton.classList.add("btn-primary")
 }
+
+function enterButton() {
+    var sku = parseInt(productSku.value)
+    var quantity = parseFloat(itemQuantity.value)
+
+    getItem(sku, function(item) {
+        order.push({
+            id: sku,
+            name: item.name,
+            unit_price: item.unit_price,
+            amount: quantity,
+            subtotal: item.unit_price * quantity
+        })
+    })
+}
+
+function getItem(id, callback) {
+    var xhr = new XMLHttpRequest()
+    xhr.onload = function() {
+        callback(JSON.parse(xhr.responseText))
+    }
+    xhr.open("GET", "/api/items/" + id, true)
+    xhr.send(null)
+}
+

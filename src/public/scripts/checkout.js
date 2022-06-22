fb0f911db4ce52c8a251b2a70627cac359d43a1a
“rjchandnani” <rishi.j.chandnani@gmail.com>
2022-06-22 20:22:25 +0200

    added the addReceipt line functionality and fixed issues with other functions (pp all)

--- src/public/scripts/checkout.js
+++ src/public/scripts/checkout.js
@@ -5,6 +5,7 @@ var cashButton = document.getElementById("cash-button")
 var cardButton = document.getElementById("card-button")
 var productSku = document.getElementById("product-sku")
 var itemQuantity = document.getElementById("item-quantity")
+var checkoutBody = document.getElementById("checkout-body")
 
 function onCash() {
     cash = true
@@ -23,13 +24,15 @@ function enterButton() {
     var quantity = parseFloat(itemQuantity.value)
 
     getItem(sku, function(item) {
-        order.push({
+        var line = {
             id: sku,
             name: item.name,
             unit_price: item.unit_price,
             amount: quantity,
             subtotal: item.unit_price * quantity
-        })
+        }
+        order.push(line)
+        addReceiptLine(line);
     })
 }
 
@@ -40,5 +43,9 @@ function getItem(id, callback) {
     }
     xhr.open("GET", "/api/items/" + id, true)
     xhr.send(null)
+}
+
+function addReceiptLine(line) {
+    checkoutBody.innerHTML += "<tr> <th scope=\"row\">" + line.name + "</th> <td>" + line.unit_price + "</td> <td>" + line.amount + " kg</td> <td>&euro;" + line.subtotal + "</td> </tr>"
 }
 

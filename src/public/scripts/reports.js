1b2f76190506b8a400052ee2c4d93405d8393840
Carlos Carrillo <90294685+cjst3ch@users.noreply.github.com>
2022-06-30 00:05:15 +0200

    updating tab pane & radio button styling

--- src/public/scripts/reports.js
+++ src/public/scripts/reports.js
@@ -1,6 +1,29 @@
 function isChecked(){
     console.log("hello")
+    if (option1.checked) {
+        document.getElementById("b-option1").classList.add("active")
+        document.getElementById("b-option2").classList.remove("active")
+        document.getElementById("b-option3").classList.remove("active")
+        document.getElementById("b-option4").classList.remove("active")
+    }
+    else if (option2.checked) {
+        document.getElementById("b-option1").classList.remove("active")
+        document.getElementById("b-option2").classList.add("active")
+        document.getElementById("b-option3").classList.remove("active")
+        document.getElementById("b-option4").classList.remove("active")
+    }
+    else if (option3.checked) {
+        document.getElementById("b-option1").classList.remove("active")
+        document.getElementById("b-option2").classList.remove("active")
+        document.getElementById("b-option3").classList.add("active")
+        document.getElementById("b-option4").classList.remove("active")
+    }
     if (option4.checked) {
+        document.getElementById("b-option1").classList.remove("active")
+        document.getElementById("b-option2").classList.remove("active")
+        document.getElementById("b-option3").classList.remove("active")
+        document.getElementById("b-option4").classList.add("active")
+
         document.getElementById("from").disabled = false;
         document.getElementById("to").disabled = false;
     }

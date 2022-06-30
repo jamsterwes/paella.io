2f0436b854e27f60c1a909327aad6e2dae8f4cc4
“rjchandnani” <rishi.j.chandnani@gmail.com>
2022-06-30 11:48:45 +0200

    added the popup table for orders and attempted to make it editable

--- src/public/scripts/orders.js
+++ src/public/scripts/orders.js
@@ -4,6 +4,8 @@ function renderRow(order) {
     <th scope="row">${order.id}</th>
     <td scope="row">&euro;${order.cost.toLocaleString('es-ES', { minimumFractionDigits: 2 })}</td>
     <td scope="row">${order.received ? "Yes" : "No"}</td>
+    <td style="text-align: center"><button data-toggle="modal" data-target="#orderViewModal" onclick="renderLines(${order.id})" class="btn btn-success border border-dark "
+            id="delete-btn"><i class="fa-solid fa-eye"></i></button></td>
     <td style="text-align: center"><a href="" class="btn btn-lg btn-delete border border-dark "
             id="delete-btn"><i class="fa-solid fa-file-circle-xmark"></i></a></td>
 </tr>`
@@ -45,3 +47,46 @@ function advanceCursor(amount) {
 }
 
 advanceCursor(0)
+
+function renderLines(id) {
+    document.getElementById("single-order-body").innerHTML = ""
+    document.getElementById("orderViewModalLabel").innerText = "View Order #" + id
+
+    getItems(items => {
+        getOrder(id, order => {
+            var out = ""
+            for (var line of order.lines) {
+                var template = `
+                <tr>
+                    <td>${items[line.item_id].display_name}</td>
+                    <td><span id="order-line-quantity-${line.item_id}">${line.quantity.toFixed(3).replace(".", ",")} ${items[line.item_id].by_weight ? "kg" : "unit"}</span></td>
+                    <td>&euro;${(line.quantity * items[line.item_id].unit_price).toFixed(2).replace(".", ",")}</td>
+                </tr>`
+                out += template + "\n"
+            }
+            document.getElementById("single-order-body").innerHTML = out
+
+            for (var line of order.lines) {
+                // Add editable quantity
+                makeEditableField("order-line-quantity-" + line.item_id, value => {
+                    // Check for invalid
+                    if (isNaN(value)) return false;
+                    // Send update to DB
+                    console.log(order.lines)
+                    updateOrder(line.item_id, { lines: [{
+                        item_id: line.item_id,
+                        quantity: value
+                    }].concat(order.lines.filter(x => x.item_id != line.item_id)) })
+                    // Valid
+                    return true;
+                }, display => {
+                    // Convert display to input
+                    return parseFloat(display.replace(",", "."))
+                }, input => {
+                    // Convert input to display
+                    return parseFloat(input).toFixed(3).replace(".", ",")
+                })
+            }
+        })
+    })
+}
